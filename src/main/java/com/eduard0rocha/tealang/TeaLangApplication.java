34823650670ad53c7e6be9f2da975c2ab50a13a2
Eduard0Rocha <zecarocha8@gmail.com>
2026-08-22 18:12:24 +0100

    Setup application

---
+++ src/main/java/com/eduard0rocha/tealang/TeaLangApplication.java
@@ -0,0 +1,19 @@
+package com.eduard0rocha.tealang;
+
+/**
+ * Entry point of tea-lang application.
+ */
+public class TeaLangApplication {
+
+	public static void main(String[] args) {
+		// Parse application arguments
+		// TODO
+		
+		// Validate application arguments
+		// TODO
+		
+		// Start CLI session
+		// TODO
+	}
+
+}
