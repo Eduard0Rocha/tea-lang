af1696b87b97e2beeabbee4c3fde6987b0cc7dfe
Eduard0Rocha <zecarocha8@gmail.com>
2026-08-23 18:06:22 +0100

    Setup KnowledgeBaseManager

--- src/main/java/com/eduard0rocha/tealang/cli/CommandLineInterface.java
+++ src/main/java/com/eduard0rocha/tealang/cli/CommandLineInterface.java
@@ -1,42 +1,27 @@
 package com.eduard0rocha.tealang.cli;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
-import com.eduard0rocha.tealang.data.TeaProgram;
-import com.eduard0rocha.tealang.parser.TeaParserFacade;
+import com.eduard0rocha.tealang.knowledgebase.KnowledgeBaseManager;
 
 /**
  * CLI interface.
  */
 public class CommandLineInterface {
+	
+	/**
+	 * Knowledge base manager.
+	 */
+	private KnowledgeBaseManager knowledgeBaseManager = new KnowledgeBaseManager();;
 
 	public CommandLineInterface() {
-		// TODO: instantiate the "KnowledgeBaseManager"
 	}
 	
 	/**
-	 * Reads and executes each Tea clause from the given file, one per line.
-	 * Blank lines and lines starting with {@code %} are skipped.
+	 * Loads the Tea program from the given file into the knowledge base.
 	 *
 	 * @param filePath the path to a .tea file
 	 */
 	public void runFile(final String filePath) {
-		final Path path = Path.of(filePath);
-		try {
-			final String content = Files.readString(path);
-	        handleFileProgram(content);
-	        // TODO: confirmation message
-		} catch (final IOException e) {
-			System.err.println("Failed to read file: " + filePath + " (" + e.getMessage() + ")");
-		}
-	}
-	
-	private void handleFileProgram(final String program) {
-	    final TeaProgram parsedProgram = TeaParserFacade.parseProgram(program);
-	    System.out.println(parsedProgram);
-	    // TODO: handle parsedProgram
+		knowledgeBaseManager.loadFile(filePath);
 	}
 	
 	/**
---
+++ src/main/java/com/eduard0rocha/tealang/knowledgebase/KnowledgeBaseManager.java
@@ -0,0 +1,39 @@
+package com.eduard0rocha.tealang.knowledgebase;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import com.eduard0rocha.tealang.data.TeaProgram;
+import com.eduard0rocha.tealang.parser.TeaParserFacade;
+
+/**
+ * Knowledge base manager.
+ */
+public class KnowledgeBaseManager {
+
+	public KnowledgeBaseManager() {
+	}
+	
+	/**
+	 * Reads, parses, and loads the Tea program from the given file into the knowledge base.
+	 *
+	 * @param filePath the path to a .tea file
+	 */
+	public void loadFile(final String filePath) {
+		final Path path = Path.of(filePath);
+		try {
+			final String content = Files.readString(path);
+	        handleFileProgram(content);
+	        // TODO: confirmation message
+		} catch (final IOException e) {
+			System.err.println("Failed to read file: " + filePath + " (" + e.getMessage() + ")");
+		}
+	}
+	
+	private void handleFileProgram(final String program) {
+	    final TeaProgram parsedProgram = TeaParserFacade.parseProgram(program);
+	    System.out.println(parsedProgram);
+	    // TODO: handle parsedProgram
+	}
+}
