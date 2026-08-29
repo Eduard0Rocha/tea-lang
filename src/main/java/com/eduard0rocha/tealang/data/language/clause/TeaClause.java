e9106ba7e7283349ba5f4326e39eeb4dcde51ba5
Eduard0Rocha <zecarocha8@gmail.com>
2026-08-29 15:47:54 +0100

    Implement TeaRule record

--- src/main/java/com/eduard0rocha/tealang/data/language/clause/TeaClause.java
+++ src/main/java/com/eduard0rocha/tealang/data/language/clause/TeaClause.java
@@ -5,7 +5,7 @@ import com.eduard0rocha.tealang.data.language.term.TeaTerm;
 /**
  * Tea clause DTO interface.
  */
-public sealed interface TeaClause permits TeaFact {
+public sealed interface TeaClause permits TeaFact, TeaRule {
 	
 	/**
 	 * Returns this clause's head term.
